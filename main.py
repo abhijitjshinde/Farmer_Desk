0aac764c4a23595ac0b6cad16d86fcc8b49b1bee
Abhijit <abhshinde703@gmail.com>
2020-07-03 21:21:04 +0530

    first satge making map work

--- main.py
+++ main.py
@@ -1,78 +1,18 @@
 from kivymd.app import MDApp
+from farmers_mapview import FarmersMapView
+import sqlite3
 
-from kivy.lang.builder import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+class MainApp(MDApp):
+    connection = None
+    cursor = None
 
+    def on_start(self):
+        # Initialize GPS
 
+        # Connect to database
+        self.connection = sqlite3.connect("markets.db")
+        self.cursor = self.connection.cursor()
 
-Layout = """
+        # Instantiate SearchPopupMenu
 
-<ContentNavigationDrawer>:
-
-    ScrollView:
-
-        MDList:
-
-            OneLineListItem:
-                text: "Screen 1"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 1"
-
-            OneLineListItem:
-                text: "Screen 2"
-                on_press:
-                    root.nav_drawer.set_state("close")
-                    root.screen_manager.current = "scr 2"
-
-
-Screen:
-
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "MDNavigationDrawer"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-
-    NavigationLayout:
-        x: toolbar.height
-
-        ScreenManager:
-            id: screen_manager
-
-            Screen:
-                name: "scr 1"
-
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
-
-            Screen:
-                name: "scr 2"
-
-                MDLabel:
-                    text: "Screen 2"
-                    halign: "center"
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            ContentNavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-
-
-"""
-class ContentNavigationDrawer(BoxLayout):
-    screen_manager = ObjectProperty()
-    nav_drawer = ObjectProperty()
-
-class Record_manager(MDApp):
-
-    def build(self):
-        return Builder.load_string(Layout)
-        
-if __name__ == "__main__":
-    Record_manager().run()
+MainApp().run()
